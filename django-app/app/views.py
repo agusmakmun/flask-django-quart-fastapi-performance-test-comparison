367c8e5620758b64cf3fbd610fb0076d20b7bd11
Agus Makmun <agus@practical-devsecops.com>
2025-04-21 16:46:02 +0700

    feat: change django view from sync to async

--- django-app/app/views.py
+++ django-app/app/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 
 
-def dummy_view(request):
+async def dummy_view(request):
     return JsonResponse(
         {"message": "Hello from Django dummy endpoint!", "status": "success"}
     )
